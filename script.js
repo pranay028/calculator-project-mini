b2516631137548790332595c3bddfbf3ca190ae4
pranay028 <pranaynomula28@gmail.com>
2021-06-05 13:40:05 +0530

    popup added and  calculator changes

--- script.js
+++ script.js
@@ -3,30 +3,32 @@ const operateEl = document.querySelectorAll(".operate");
 const buttonsEl = document.querySelectorAll(".btn");
 const button = document.querySelector('.btn')
 
-const oneEl = document.getElementById("one")
-const twoEl = document.getElementById("two")
-const threeEl = document.getElementById("three")
-const fourEl = document.getElementById("four")
-const fiveEl = document.getElementById("five")
-const sixEl = document.getElementById("six")
-const sevenEl = document.getElementById("seven")
-const eightEl = document.getElementById("eight")
-const nineEl = document.getElementById("nine")
-const dotEl = document.getElementById("dot")
-const clearEl = document.getElementById("clear")
-const multiEl = document.getElementById("multi")
-const divideEl = document.getElementById("divide")
-const percentileEl = document.getElementById("percentile")
-const subtractEl = document.getElementById("subtract")
-const addEl = document.getElementById("add")
-const resultEl = document.getElementById("result")
+// const oneEl = document.getElementById("one")
+// const twoEl = document.getElementById("two")
+// const threeEl = document.getElementById("three")
+// const fourEl = document.getElementById("four")
+// const fiveEl = document.getElementById("five")
+// const sixEl = document.getElementById("six")
+// const sevenEl = document.getElementById("seven")
+// const eightEl = document.getElementById("eight")
+// const nineEl = document.getElementById("nine")
+// const dotEl = document.getElementById("dot")
+// const clearEl = document.getElementById("clear")
+// const multiEl = document.getElementById("multi")
+// const divideEl = document.getElementById("divide")
+// const percentileEl = document.getElementById("percentile")
+// const subtractEl = document.getElementById("subtract")
+// const addEl = document.getElementById("add")
+// const resultEl = document.getElementById("result")
 
 const displayaddEl = document.getElementById("displayadd")
 
 // display....
 const displayEl = document.querySelector(".display");
 
-
+const closeEl = document.querySelector(".close");
+const helpEl = document.querySelector(".help");
+const instructionEl = document.querySelector(".instructions");
 
 displayEl.textContent = "";
 // let x = 0;
@@ -35,120 +37,141 @@ let totalValue = 0;
 let value = 0;
 let listOperated = [];
 let idArray = [];
+let totalArray = []
 
-for(let i = 0; i < buttonsEl.length; i++){
-    var x;
-    var y;
+closeEl.addEventListener("click", function(){
+    instructionEl.classList.add("hidden");
+    // instructionEl.style.animation = "popdown 400ms 1";
+})
+document.addEventListener("keydown", function(e){
+    if(e.key == "Escape"){
+        instructionEl.classList.add("hidden");
+        // instructionEl.style.animation = "popdown 400ms 1";
+    }
+})
+helpEl.addEventListener("click", function(){
     
-    buttonsEl[i].addEventListener("click", function(){
-        
-        if(buttonsEl[i].id == 'clear'){
-            x = 0;
-            listOperated = [0, 0]
-            displayEl.textContent = "";
-            displayaddEl.textContent = "";
-            value = 0;
-            // totalValue = 0;
+    instructionEl.classList.toggle("hidden");
+    
+})
 
-        }else if(buttonsEl[i].className.includes("number")){
-            // console.log(buttonsEl[i].id);
+    for(let i = 0; i < buttonsEl.length; i++){
+        var x;
+        var y;
+        
+        buttonsEl[i].addEventListener("click", function(){
             
-            displayEl.textContent += buttonsEl[i].textContent;
-            value = Number(displayEl.textContent);
-            
-        }else if(buttonsEl[i].className.includes("operate") && buttonsEl[i].id != "result"){
-            // x = value;
-                                 
-            let id = buttonsEl[i].id;
-            // let forID = buttonsEl[i].classList.add("active");
-            console.log(id);
-            
-            console.log(listOperated);
-            // console.log(idArray[idArray.length - 1]);
-            // listOperated.push(value)
-            if(idArray.length >= 1){
-                if(buttonsEl[i].id != idArray[idArray.length - 1]){
-                    value = 0;
-                    displayaddEl.textContent = totalValue;
+            if(buttonsEl[i].id == 'clear'){
+                x = 0;
+                // listOperated = [];
+                displayEl.textContent = "";
+                displayaddEl.textContent = "";
+                value = 0;
+                totalValue = 0;
+
+            }else if(buttonsEl[i].className.includes("number")){
+                // console.log(buttonsEl[i].id);
+                // displayEl.textContent = "";
+                if(displayEl.textContent == totalValue) {
                     displayEl.textContent = "";
-                    newValue = totalValue;
-                    
+                }
+                displayEl.textContent += buttonsEl[i].textContent;
+                value = Number(displayEl.textContent);
+                
+            }else if(buttonsEl[i].className.includes("operate") && buttonsEl[i].id != "result"){
+                // x = value;
+                                    
+                let id = buttonsEl[i].id;
+                // let forID = buttonsEl[i].classList.add("active");
+                console.log(id);
+                
+                console.log(listOperated);
+                // console.log(idArray[idArray.length - 1]);
+                // listOperated.push(value)
+                if(idArray.length >= 1){
+                    if(buttonsEl[i].id != idArray[idArray.length - 1]){
+                        value = 0;
+                        displayaddEl.textContent = totalValue;
+                        displayEl.textContent = "";
+                        newValue = totalValue;
+                        
+                    }else{
+                        newValue = listOperated[listOperated.length - 1];
+                        displayaddEl.textContent = newValue;
+                        displayEl.textContent = "";
+                        listOperated.push(value);
+                        
+                    }
                 }else{
-                    newValue = listOperated[listOperated.length - 1];
-                    displayaddEl.textContent = newValue;
-                    displayEl.textContent = "";
                     listOperated.push(value);
+                    newValue = value;
                     
                 }
-            }else{
-                listOperated.push(value);
-                newValue = value;
-                
-            }
-            idArray.push(buttonsEl[i].id); 
-            console.log(idArray[idArray.length - 1]);
+                idArray.push(buttonsEl[i].id); 
+                // console.log(idArray[idArray.length - 1]);
 
-            // listOperated.push(value)
-            displayaddEl.textContent = newValue;
-            displayEl.textContent = "";
-            
-        }else if(buttonsEl[i].id == 'result'){
-            if(idArray[idArray.length - 1] == "add"){
+                // listOperated.push(value)
+                displayaddEl.textContent = newValue;
+                displayEl.textContent = "";
                 
-                totalValue = newValue + value;
-                // displayaddEl.textContent = totalValue
+            }else if(buttonsEl[i].id == 'result'){
+                if(idArray[idArray.length - 1] == "add"){
+                    
+                    totalValue = newValue + value;
+                    // displayaddEl.textContent = totalValue
+                    
+                    // listOperated.push(totalValue)
+                }else if(idArray[idArray.length - 1] == "subtract") {
+                    
+                    totalValue = newValue - value;
+                    // displayaddEl.textContent = totalValue
+                    // displayEl.textContent = totalValue;
+                    // listOperated.push(totalValue)}
+                }else if(idArray[idArray.length - 1] == "multi") {
+                    console.log(newValue, value);
+                    totalValue = newValue * value;
+                    console.log(totalValue);
+
+                }else if(idArray[idArray.length - 1] == "divide"){
+                    // if(listOperated[0] === 0) totalValue = 1;
+                    console.log(newValue, value);
+                    totalValue = newValue / value;
+                    console.log(totalValue);
+                }else if(idArray[idArray.length - 1] == "percentile"){
+                    // if(listOperated[0] === 0) totalValue = 1;
+                    console.log(newValue, value);
+                    totalValue = newValue % value;
+                    console.log(totalValue);
+                }
                 
-                // listOperated.push(totalValue)
-            }else if(idArray[idArray.length - 1] == "subtract") {
-                
-                totalValue = newValue - value;
-                // displayaddEl.textContent = totalValue
                 // displayEl.textContent = totalValue;
-                // listOperated.push(totalValue)}
-            }else if(idArray[idArray.length - 1] == "multi") {
-                if(listOperated[0] === 0) totalValue = 1;
-                totalValue = newValue * value;
-                //  displayaddEl.textContent = totalValue
-                // displayEl.textContent = totalValue;
-                // listOperated.push(totalValue)
+                listOperated.push(totalValue)
+                displayaddEl.textContent = totalValue;
+                displayEl.textContent = totalValue;
 
-            }else if(idArray[idArray.length - 1] == "divide"){
-                if(listOperated[0] === 0) totalValue = 1;
-                totalValue = newValue / value;
-            }else if(idArray[idArray.length - 1] == "divide"){
-                // if(listOperated[0] === 0) totalValue = 1;
-                totalValue = newValue % value;
+            } 
+            if(buttonsEl[i].id == 'lv'){
+                document.querySelector(".sidedisplay").innerHTML = totalValue;
 
             }
+                
+                // console.log(value);
+                // console.log(totalValue);
+                // console.log(listOperated);
             
-            // displayEl.textContent = totalValue;
-            listOperated.push(totalValue)
-            displayaddEl.textContent = totalValue;
-            displayEl.textContent = totalValue;
+                
+                // let forID = buttonsEl[i].classList.add("active");
 
-        } 
-        if(buttonsEl[i].id == 'lv'){
-            document.querySelector(".sidedisplay").innerHTML = totalValue;
-
-        }
+                // let calc = buttonsEl[i].id
+                    
+                
+            // console.log(idArray);  
+            })
             
-            console.log(value);
-            console.log(totalValue);
-            console.log(listOperated);
-          
-            
-            // let forID = buttonsEl[i].classList.add("active");
-
-             // let calc = buttonsEl[i].id
-                 
-            
-        console.log(idArray);  
-        })
-        
-        // let id = buttonsEl[i].id;
-        // displayEl.textContent += buttonsEl[i].textContent
-        //  console.log(displayEl.textContent);
-}
+            // let id = buttonsEl[i].id;
+            // displayEl.textContent += buttonsEl[i].textContent
+            //  console.log(displayEl.textContent);
+    }
     
 
 // const calcMulti = function(x, y){
